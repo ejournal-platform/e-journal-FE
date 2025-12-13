5e78c02f933ae1e73c6ff416f8b440a1ac3d63a0
dulaj96 <dulajmithun96@gmail.com>
2025-12-13 22:44:24 +0530

    updated UI

--- src/pages/eJournal/announcement/AnnouncementsPage.tsx
+++ src/pages/eJournal/announcement/AnnouncementsPage.tsx
@@ -12,10 +12,10 @@ interface Announcement {
 const mockAnnouncements: Announcement[] = [
   {
     id: 1,
-    date: "July 26, 2024",
-    title: "New Food Safety Guidelines Released",
+    date: "",
+    title: "",
     description:
-      "The Ministry of Health has issued updated guidelines on food safety, effective immediately. All trainers are required to review and incorporate these changes into their training materials.",
+      "",
     iconSvg: (
       <img
         src={''}
